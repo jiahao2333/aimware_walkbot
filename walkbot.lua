2f01b7f249aeb0b39a7fb27303898b490f73dcf8
ShadyRetard <shady@horseporn.org>
2020-03-29 21:52:08 +0200

    Fix loading of walkbot after update

--- walkbot.lua
+++ walkbot.lua
@@ -4,7 +4,7 @@ walkbot.config = RunScript("walkbot\\modules\\config.lua")
 walkbot.json = RunScript(walkbot.config.main_directory .. "\\json.lua")
 
 for _, value in pairs(walkbot.config.modules) do
-    walkbot[value] = RunScript(walkbot.config.modules_path .. "\\" .. value .. ".lua")
+    walkbot[value] = RunScript(walkbot.config.modules_directory .. "\\" .. value .. ".lua")
 end
 
 for _, value in pairs(walkbot.config.modules) do
